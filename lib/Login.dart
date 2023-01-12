be7af1a6ba2b01223853a5acd48548f0265097c8
Victor323232 <victorhugoabc59@gmail.com>
2023-01-12 16:07:09 -0300

    added color background and logo

--- lib/Login.dart
+++ lib/Login.dart
@@ -10,11 +10,29 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("nomes"),
-      ),
       body: Container(
+        decoration: BoxDecoration(color: Color(0xff075E54)),
+        padding: EdgeInsets.all(16),
+        child:Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("images/logo.png",
+                  width: 200,
+                  height: 250,
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(bottom: 8),
 
+
+              ),
+            ],
+          ),
+        )
+        ),
       ),
     );
   }
